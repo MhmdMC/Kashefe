3a8f204d5639c83a574f38eddf2054e17ed1bd98
MhmdMC <121291901+MhmdMC@users.noreply.github.com>
2025-08-20 20:42:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,11 @@ def index():
 
     return render_template("index.html", today=date.today().isoformat())
 
+@app.route("/uptime", methods=["GET", "POST"])
+def index():
+    return render_template("uptime.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
+
 
